eea7e09373d259edb566b310bbef0013070ec179
Jeremiah <jeremiahmarcos27@outlook.com>
2021-12-15 15:49:57 -0600

    completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+    def show
+        user_items = User.find(params[:id])
+        render json: user_items, include: :items, status: :ok
+    end
 end
